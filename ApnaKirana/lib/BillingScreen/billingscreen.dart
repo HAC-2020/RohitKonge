f73f3acc7f762573e48fe4ee5a2afdb499c54c77
RohitKonge <rohitkonge08@gmail.com>
2020-08-08 22:44:56 +0530

    added bill screen

--- ApnaKirana/lib/BillingScreen/billingscreen.dart
+++ ApnaKirana/lib/BillingScreen/billingscreen.dart
@@ -9,7 +9,134 @@ class MyBillingScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Bill'),
       ),
-      // child: Text('Sample'),
+      body: CodeForThis(),
+    );
+  }
+}
+
+class CodeForThis extends StatefulWidget {
+  CodeForThis({Key key}) : super(key: key);
+
+  @override
+  _CodeForThisState createState() => _CodeForThisState();
+}
+
+class _CodeForThisState extends State<CodeForThis> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.text,
+                // controller: _phoneController,
+                decoration: InputDecoration(
+                  hintText: 'Store Name',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.greenAccent),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  // controller: _phoneController,
+                  decoration: InputDecoration(
+                    hintText: 'Address of Store',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.greenAccent),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        // controller: _phoneController,
+                        decoration: InputDecoration(
+                          hintText: 'Mobile Number',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.greenAccent),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        // controller: _phoneController,
+                        decoration: InputDecoration(
+                          hintText: 'Date',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.greenAccent),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 400,
+                color: Colors.grey,
+                child: ListView(
+                  children: <Widget>[
+                    Text('asdf'),
+                    Text('asdf'),
+                    Text('asdf'),
+                  ],
+                ),
+              ),
+              FlatButton(onPressed: () {}, child: Text('asfasfasf'))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
